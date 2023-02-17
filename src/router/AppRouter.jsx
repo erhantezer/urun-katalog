e3ff79b694a6bda39703095f1e0b14e53484291f
Erhan <erhantezer68@gmail.com>
2023-02-17 21:44:41 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,16 +5,19 @@ import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import CardItem from "../pages/CardItem";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="card/:id" element={<CardItem/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="card/:id" element={<PrivateRouter/>}>
+          <Route path="" element={<CardItem />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
