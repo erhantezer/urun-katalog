399b2ddbe875e088ba1b351159e7c03977b5f279
Erhan <erhantezer68@gmail.com>
2023-02-17 14:29:01 +0300

    updated

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRouter = () => {
+    const currentUser = true;
+    return currentUser ? <Outlet /> : <Navigate to="/login" replace />
+};
+
+export default PrivateRouter;
