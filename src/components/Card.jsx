a43013c3c574b40b2b122e3900d50cbffea4e726
Erhan <erhantezer68@gmail.com>
2023-02-23 04:19:46 +0300

    card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 const Card = ({ id, name, image, description }) => {
     const navigate = useNavigate()
-    console.log(image)
+    const [show, setShow] = useState(false);
+    
+    const defaultImage = "https://picsum.photos/500/300?random=1"
+console.log(defaultImage)
+    const handleClick = () => {
+        navigate(`card/${id}`)
+    }
+
 
     return (
-        <div className="flex justify-center mt-8 cursor-pointer" key={id} >
-            <div className="rounded-lg shadow-lg bg-white max-w-sm" onClick={navigate(`/card/${id}`)}>
-                <img className="rounded-t-lg" src={image} alt="" />
+        <div style={{height:"42rem"}} className="flex justify-center mt-8 cursor-pointer" key={id} >
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                <img className="rounded-t-lg" src={!image ? image : defaultImage} alt="" onClick={handleClick}/>
 
 
                 <div className="p-6">
-                    <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
-                    <p className="text-gray-700 text-base mb-4">
-                        {description}
+                    <h5 className="text-gray-900 text-xl font-medium mb-2 ">{name}</h5>
+                    <p className="text-gray-700 text-base mb-4 mt-4">
+                        {show ? description : `${description.slice(0, 80)} ...`}
+                        <br />
+                        <button className="bg-blue-200 p-1 rounded-lg" onClick={() =>setShow(!show)}>
+                        {show ?  " Show Less" : " Show More"}
+                        </button>
+                        
                     </p>
 
                 </div>
